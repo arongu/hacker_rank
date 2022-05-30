d26e1adb7da2fd8f21fa6be8335631cc01f19945
Aron Gulyas <aron.gulyas.hu@genesys.com>
2022-05-30 14:46:25 +0200

    BalancedParantheses working, need to fix mvn test

--- src/main/java/aron/hacker_rank/BalancedParentheses.java
+++ src/main/java/aron/hacker_rank/BalancedParentheses.java
@@ -3,37 +3,48 @@ package aron.hacker_rank;
 import java.util.Stack;
 
 public class BalancedParentheses {
-    public static final char[][] terms = {{ '(', ')' }, { '[', ']' }, { '{', '}'}};
+    public static final char[] openings = { '(', '{', '[' };
+    public static final char[] closings = { ')', '}', ']' };
 
-    public static boolean isOpenTerm(char openTerm) {
-        for (char[] arr : terms) {
-            if ( openTerm == arr[0]){
-                return true;
+    public static boolean isOpening(final char character) {
+        for ( char op : openings) {
+            if ( character == op) return true;
+        }
+
+        return false;
+    }
+
+    public static boolean isClosing(final char character) {
+        for ( char clo : closings) {
+            if ( character == clo) return true;
+        }
+
+        return false;
+    }
+
+    public static boolean areTheyMatch(char opening, char closing) {
+        for ( int i = 0; i < openings.length; i++) {
+            if (opening == openings[i]) {
+                return closing == closings[i];
             }
         }
 
         return false;
     }
 
-    public static boolean areTheyMatch(char openedTerm, char closedTerm) {
-        for (char[] arr : terms) {
-            if ( openedTerm == arr[0]){
-                return closedTerm == arr[1];
-            }
-        }
-
-        return false;
-    }
-
-    public boolean isBalanced(final String expression) {
-        Stack<Character> stack = new Stack<>();
-        for (char c : expression.toCharArray()) {
-            if ( isOpenTerm(c)) {
+    public static boolean isBalanced(final String expression) {
+        final Stack<Character> stack = new Stack<>();
+        for ( char c : expression.toCharArray()) {
+            if ( isOpening(c)) {
                 stack.push(c);
-            } else {
-                if ( stack.isEmpty() || ! areTheyMatch(stack.pop(), c)){
-                    return false;
-                }
+                System.out.println(stack);
+            }
+            else if ( isClosing(c)) {
+                if ( stack.isEmpty() ) return false; // if there were no previously stored openings -> it is already malformed e.g.: ]
+                if ( !areTheyMatch(stack.pop(), c)) return false; // get the immediate opening and check if it matches to the closing if not -> malformed
+                System.out.println(stack);
+                // note: ([{((((([{} ... no matter how many openings you have, at some point you have to close it regardless how deep is the nest,
+                // the first 'closing' must match with the last opening character
             }
         }
 
--- src/test/java/aron/hacker_rank/BalancedParenthesesTest.java
+++ src/test/java/aron/hacker_rank/BalancedParenthesesTest.java
@@ -1,4 +1,32 @@
 package aron.hacker_rank;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class BalancedParenthesesTest {
+    @Test
+    public void isBalanced_shouldReturnTrue_whenStringHasNoOpenOrClosedTerms() {
+        final String s = "this is my string";
+        assertTrue(BalancedParentheses.isBalanced(s));
+    }
+
+    @Test
+    public void isBalanced_shouldReturnFalse_whenStringHasOnlyOneOpeningParentheses() {
+        final String s = "this is my (string";
+        assertFalse(BalancedParentheses.isBalanced(s));
+    }
+
+    @Test
+    public void isBalanced_shouldReturnTrue_whenStringHasTwoOneOpeningParentheses() {
+        final String s = "this is my (string)";
+        assertTrue(BalancedParentheses.isBalanced(s));
+    }
+
+    @Test
+    public void isBalanced_shouldReturnTrue_whenStringIsBalanced() {
+        final String s = "text{[{[((text))]}]}text";
+        assertTrue(BalancedParentheses.isBalanced(s));
+    }
 }
