82f2fb33edd7927230ad05c51440e9345afdbad9
Aron Gulyas <aron.gulyas.hu@genesys.com>
2022-06-01 08:17:14 +0200

    BalancedGyak2

---
+++ src/test/java/aron/hacker_rank/gyak/BalancedGyak2Test.java
@@ -0,0 +1,38 @@
+package aron.hacker_rank.gyak;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BalancedGyak2Test {
+    @Test
+    public void isBalanced_shouldReturnTrue_whenStringHasNoOpenOrClosedTerms() {
+        final String s = "this is my string";
+        assertTrue(BalancedGyak2.isBalanced(s));
+    }
+
+    @Test
+    public void isBalanced_shouldReturnFalse_whenStringHasOnlyOneOpeningParentheses() {
+        final String s = "this is my (string";
+        assertFalse(BalancedGyak2.isBalanced(s));
+    }
+
+    @Test
+    public void isBalanced_shouldReturnFalse_whenStringHasOnlyOneOpeningParenthesesAtTheEnd() {
+        final String s = "this is my string(";
+        assertFalse(BalancedGyak2.isBalanced(s));
+    }
+
+    @Test
+    public void isBalanced_shouldReturnTrue_whenStringHasTwoOneOpeningParentheses() {
+        final String s = "this is my (string)";
+        assertTrue(BalancedGyak2.isBalanced(s));
+    }
+
+    @Test
+    public void isBalanced_shouldReturnTrue_whenStringIsBalanced() {
+        final String s = "text{[{[((text))]}]}text";
+        assertTrue(BalancedGyak2.isBalanced(s));
+    }
+}
