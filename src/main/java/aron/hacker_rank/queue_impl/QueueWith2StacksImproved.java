a36cd6702b1df2d2f203cc41670b7f228cc979a4
Aron Gulyas <aron.gulyas.hu@gmail.com>
2022-06-28 15:11:22 +0200

    improved 2 stacks implementation

---
+++ src/main/java/aron/hacker_rank/queue_impl/QueueWith2StacksImproved.java
@@ -0,0 +1,38 @@
+package aron.hacker_rank.queue_impl;
+
+import java.util.Stack;
+
+public class QueueWith2StacksImproved<T> {
+    private final Stack<T> inputStack  = new Stack<>();
+    private final Stack<T> outputStack = new Stack<>();
+
+    private void shiftStack() {
+        if ( outputStack.isEmpty() ) {
+            while ( ! inputStack.isEmpty() ) {
+                outputStack.push(inputStack.pop());
+            }
+        }
+    }
+
+    // enqueue
+    public void enqueue(final T value) {
+        inputStack.push(value);
+    }
+
+    // dequeue
+    public T dequeue() {
+        shiftStack();
+        return outputStack.pop();
+    }
+
+    // peek
+    public T peek() {
+        shiftStack();
+        return outputStack.peek();
+    }
+
+    // size
+    public int size() {
+        return inputStack.size() + outputStack.size();
+    }
+}
