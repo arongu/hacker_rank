6f1d54b890d99cbd932c8f882ea28302e9eb8cb6
Aron Gulyas <aron.gulyas.hu@gmail.com>
2022-07-15 12:15:01 +0200

    balanced exercise

---
+++ src/main/java/aron/hacker_rank/balanced_parentheses/exercises/ParenthesesStack3.java
@@ -0,0 +1,38 @@
+package aron.hacker_rank.balanced_parentheses.exercises;
+
+import java.util.Stack;
+
+public class ParenthesesStack3 {
+    public static boolean isOpening(final char c) {
+        return c == '(' || c == '[' || c == '{';
+    }
+
+    public static boolean isClosing(final char c) {
+        return c == ')' || c == ']' || c == '}';
+    }
+
+    public static boolean doTheyMatch(final char opening, final char closing) {
+        if ( opening == closing ) return false;
+        if ( opening == '(' && closing == ')' ) return true;
+        if ( opening == '[' && closing == ']' ) return true;
+        return  opening == '{' && closing == '}';
+    }
+
+    public static boolean isBalanced(final String string) {
+        if ( string == null ) return false;
+
+        final char[] arr = string.toCharArray();
+        final Stack<Character> openings = new Stack<>();
+
+        for ( char c : arr ) {
+            if ( isOpening(c) ) {
+                openings.push(c);
+
+            } else if ( isClosing(c) ) {
+                if ( openings.isEmpty() || !doTheyMatch(openings.pop(), c) ) return false;
+            }
+        }
+
+        return openings.isEmpty();
+    }
+}
---
+++ src/test/java/aron/hacker_rank/balanced_parentheses/exercises/ParenthesesStack3Test.java
@@ -0,0 +1,38 @@
+package aron.hacker_rank.balanced_parentheses.exercises;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ParenthesesStack3Test {
+    public void isBalanced_shouldReturnTrue_whenStringHasNoOpenOrClosedTerms() {
+        final String s = "this is my string";
+        Assertions.assertTrue(ParenthesesStack3.isBalanced(s));
+    }
+
+    @Test
+    public void isBalanced_shouldReturnFalse_whenStringHasOnlyOneOpeningParentheses() {
+        final String s = "this is my (string";
+        assertFalse(ParenthesesStack3.isBalanced(s));
+    }
+
+    @Test
+    public void isBalanced_shouldReturnFalse_whenStringHasOnlyOneOpeningParenthesesAtTheEnd() {
+        final String s = "this is my string(";
+        assertFalse(ParenthesesStack3.isBalanced(s));
+    }
+
+    @Test
+    public void isBalanced_shouldReturnTrue_whenStringHasTwoOneOpeningParentheses() {
+        final String s = "this is my (string)";
+        assertTrue(ParenthesesStack3.isBalanced(s));
+    }
+
+    @Test
+    public void isBalanced_shouldReturnTrue_whenStringIsBalanced() {
+        final String s = "text{[{[((text))]}]}text";
+        assertTrue(ParenthesesStack3.isBalanced(s));
+    }
+}
