a84f29855224907d27547f4d2993ac3c7c070fc3
Aron Gulyas <aron.gulyas.hu@gmail.com>
2022-07-16 12:06:59 +0200

    save() -- hacker_rank -- 2022-07-16 12:06:59

---
+++ src/main/java/aron/hacker_rank/trie/TrieProto.java
@@ -0,0 +1,62 @@
+package aron.hacker_rank.trie;
+
+public class TrieProto {
+    private static class Node {
+        public char c;
+        public boolean isWord;
+        public Node[] children;
+
+        public Node(final char c) {
+            this.c   = c;
+            isWord   = false;
+            children = new Node[26];
+        }
+    }
+
+    private final Node root;
+
+    public TrieProto() {
+        root = new Node('\0');
+    }
+
+    public void insert(final String word) {
+        Node current = root;
+        for ( int i = 0; i < word.length(); i++ ) {
+            char c = word.charAt(i);
+
+            if ( current.children[ c - 'a' ] == null ) {
+                current.children[c - 'a'] = new Node(c);
+
+            } else {
+                current = current.children[ c - 'a'];
+            }
+        }
+
+        current.isWord = true;
+    }
+
+    private Node getNode(final String word) {
+        Node current = root;
+        for ( int i = 0; i < word.length(); i++ ) {
+            char c = word.charAt(i);
+
+            if ( current.children[ c - 'a' ] == null ) {
+                return null;
+
+            } else {
+                current = current.children[ c - 'a' ];
+            }
+        }
+
+        return current;
+    }
+
+    public boolean search(final String word) {
+        Node node =  getNode(word);
+        return node != null && node.isWord;
+    }
+
+    public boolean startsWith(final String prefix) {
+        return getNode(prefix) != null;
+    }
+}
