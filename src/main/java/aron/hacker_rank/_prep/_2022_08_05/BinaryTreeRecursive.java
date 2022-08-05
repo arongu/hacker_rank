7901e7b14c11b7ae17336ce236e18205e3a3049d
Aron Gulyas <aron.gulyas.hu@gmail.com>
2022-08-05 17:59:04 +0200

    BinaryTreeRecursive

---
+++ src/main/java/aron/hacker_rank/_prep/_2022_08_05/BinaryTreeRecursive.java
@@ -0,0 +1,42 @@
+package aron.hacker_rank._prep._2022_08_05;
+
+public class BinaryTreeRecursive {
+    private BinaryTreeRecursive left, right;
+    private final int number;
+
+    public BinaryTreeRecursive(final int number) {
+        this.number = number;
+    }
+
+    public boolean contains(final int n) {
+        if ( n == number ) {
+            return true;
+
+        } else if ( n < number ) {
+            if ( left == null ) return false;
+            return left.contains(n);
+
+        } else {
+            if ( right == null ) return false;
+            return right.contains(n);
+        }
+    }
+
+    public void add(final int n) {
+        if ( n < number ) {
+            if ( left == null ) {
+                left = new BinaryTreeRecursive(n);
+            } else {
+                left.add(n);
+            }
+
+        } else if ( n > number ) {
+            if ( right == null ) {
+                right = new BinaryTreeRecursive(n);
+            } else {
+                right.add(n);
+            }
+
+        }
+    }
+}
