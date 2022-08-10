7b502ac45a3aa490b04abfc845f144036ae59b19
Aron Gulyas <aron.gulyas.hu@gmail.com>
2022-08-10 09:19:13 +0200

    trie with remove again

---
+++ src/main/java/aron/hacker_rank/_prep/_2022_08_10/Trie.java
@@ -0,0 +1,135 @@
+package aron.hacker_rank._prep._2022_08_10;
+
+import java.util.Stack;
+
+public class Trie {
+    // static methods
+    private static boolean isStringBad(final String s) {
+        return s == null || s.isEmpty();
+    }
+
+    private static int getIndex(final char c) {
+        return c - 'a';
+    }
+
+    private static boolean isSafeToDelete(final Trie trie) {
+        if ( trie == null || trie.leafs == null ) return true;
+
+        for ( int i = 0; i < 26; i++ ) {
+            if ( trie.leafs[i] != null ) return false;
+        }
+
+        return true;
+    }
+
+    // class properties
+    private Trie[] leafs;
+    private final char value;
+
+    // constructors
+    private Trie(final char value) {
+        this.value = value;
+    }
+
+    public Trie() {
+        this('/');
+    }
+
+    // private utility
+    private Trie getNode(final char c) {
+        return leafs != null ? leafs[getIndex(c)] : null;
+    }
+
+    private Trie getNode(final String s) {
+        if (isStringBad(s)) return null;
+
+        Trie pos = this;
+        for ( char c : s.toCharArray() ) {
+            pos = pos.leafs[getIndex(c)];
+            if ( pos == null ) break;
+        }
+
+        return pos;
+    }
+
+    private Stack<Trie> getVisitedNodes(final String s) {
+        final Stack<Trie> visited = new Stack<>();
+        visited.push(this);
+
+        if ( isStringBad(s) ) return visited;
+
+        Trie curr = this;
+        for ( char c : s.toCharArray() ) {
+            curr = curr.getNode(c);
+            if ( curr == null ) break;
+            visited.push(curr);
+        }
+
+        return visited;
+    }
+
+    private Trie addLeaf(final char c) {
+        int index = getIndex(c);
+        Trie leaf;
+
+        if ( leafs == null ) {
+            leafs = new Trie[26];
+            leaf = new Trie(c);
+            leafs[index] = leaf;
+
+        } else {
+            leaf = leafs[index];
+            if ( leaf == null ) {
+                leaf = new Trie(c);
+                leafs[index] = leaf;
+            }
+        }
+
+        return leaf;
+    }
+
+    // public methods
+    // read
+    public char getLetter() {
+        return value;
+    }
+
+    public boolean contains(final String s) {
+        return getNode(s) != null;
+    }
+
+    public Trie lookup(final String s) {
+        return getNode(s);
+    }
+
+    // write
+    public void insert(final String s) {
+        if (isStringBad(s)) return;
+
+        Trie pos = this;
+        for ( char c : s.toCharArray() ) {
+            pos = pos.addLeaf(c);
+        }
+    }
+
+    public void remove(final String s) {
+        if ( isStringBad(s) ) return;
+        final Stack<Trie> visited = getVisitedNodes(s);
+
+        if ( visited.size() - 1 != s.length() ) return;
+
+        Trie parent, child = visited.pop();
+        while ( ! visited.isEmpty() ) {
+            parent = visited.pop();
+
+            if ( isSafeToDelete(child) ) {
+                System.out.printf("delete %c\n", child.value);
+                parent.leafs[getIndex(child.value)] = null;
+                child = parent;
+
+            } else {
+                return;
+            }
+        }
+    }
+}
---
+++ src/test/java/aron/hacker_rank/_prep/_2022_08_10/TrieTest.java
@@ -0,0 +1,96 @@
+package aron.hacker_rank._prep._2022_08_10;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TrieTest {
+
+    @Test
+    public void basics() {
+        final Trie trie = new Trie();
+        trie.insert("alma");
+        trie.insert("abba");
+        trie.insert("abban");
+        trie.insert("bal");
+        trie.insert("balaton");
+        trie.insert("balta");
+        trie.insert("mar");
+        trie.insert("marha");
+        trie.insert("marok");
+        trie.insert("martaloc");
+        trie.insert("malac");
+        trie.insert("malacka");
+        trie.insert("kerek");
+        trie.insert("kerepel");
+        trie.insert("kerekpar");
+        trie.insert("kerekparos");
+        trie.insert("tej");
+        trie.insert("tejfol");
+        trie.insert("tejfolos");
+        trie.insert("tejes");
+        trie.insert("tejbegriz");
+        trie.insert("telefon");
+        trie.insert("teljes");
+
+        assertTrue(trie.contains("alma"));
+        assertFalse(trie.contains("teja"));
+        assertTrue(trie.contains("teljes"));
+        assertEquals('l', trie.lookup("bal").getLetter());
+        assertEquals('z', trie.lookup("tejbegriz").getLetter());
+    }
+
+    @Test
+    public void testRemove() {
+        final Trie trie = new Trie();
+        trie.insert("alma");
+        trie.insert("abba");
+        trie.insert("abban");
+        trie.insert("bal");
+        trie.insert("balaton");
+        trie.insert("balta");
+        trie.insert("mar");
+        trie.insert("marha");
+        trie.insert("marok");
+        trie.insert("martaloc");
+        trie.insert("malac");
+        trie.insert("malacka");
+        trie.insert("kerek");
+        trie.insert("kerepel");
+        trie.insert("kerekpar");
+        trie.insert("kerekparos");
+        trie.insert("tej");
+        trie.insert("tejfol");
+        trie.insert("tejfolos");
+        trie.insert("tejes");
+        trie.insert("tejbegriz");
+        trie.insert("telefon");
+        trie.insert("teljes");
+
+
+        assertTrue(trie.contains("alma"));
+        assertFalse(trie.contains("teja"));
+        assertTrue(trie.contains("teljes"));
+        assertEquals('l', trie.lookup("bal").getLetter());
+        assertEquals('z', trie.lookup("tejbegriz").getLetter());
+
+        trie.remove("tej");
+        assertTrue(trie.contains("tej"));
+        trie.remove("tejes");
+        assertFalse(trie.contains("tejes"));
+        assertTrue(trie.contains("tej"));
+        assertTrue(trie.contains("tejfol"));
+        trie.insert("tejes");
+        assertTrue(trie.contains("tejes"));
+    }
+
+    @Test
+    public void testRemoveMinimal() {
+        final Trie trie = new Trie();
+        trie.insert("alma");
+
+        assertTrue(trie.contains("alma"));
+        trie.remove("alma");
+        assertFalse(trie.contains("alma"));
+    }
+}
