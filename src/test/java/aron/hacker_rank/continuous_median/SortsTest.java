c6449cf8d67bd3498a7f36adb417b904a873a16e
Aron Gulyas <aron.gulyas.hu@gmail.com>
2022-12-09 12:36:06 +0100

    more sorts

--- src/test/java/aron/hacker_rank/continuous_median/SortsTest.java
+++ src/test/java/aron/hacker_rank/continuous_median/SortsTest.java
@@ -9,22 +9,23 @@ public class SortsTest {
     public void sortTest(){
         int[] arr1 = { 1 };
         int[] arr2 = { 4, 12, 8, 5, 1, 100, 14 };
-        int[] arr3 = { 4, 12, 13, 16 };
+        int[] arr3 = { 16, 4, 13, 12 };
 
         Sorts.bubbleSort(arr1, false);
         assertEquals(1, Sorts.findMin(arr1));
         assertEquals(1, Sorts.findMax(arr1));
         assertEquals(1, Sorts.findMedian(arr1));
 
-
         Sorts.bubbleSort(arr2, false);
         assertEquals(1, Sorts.findMin(arr2));
         assertEquals(100, Sorts.findMax(arr2));
         assertEquals(8, Sorts.findMedian(arr2));
 
+        Sorts.bubbleSort(arr3, false);
         assertEquals(12.5, Sorts.findMedian(arr3));
 
         Sorts.printArray(arr1);
         Sorts.printArray(arr2);
+        Sorts.printArray(arr3);
     }
 }
