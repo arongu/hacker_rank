9a904497ee58a3bcaa80e5d8d94dc8aaaf5f1b64
Aron Gulyas <aron.gulyas.hu@gmail.com>
2022-12-09 12:30:16 +0100

    median

--- src/main/java/aron/hacker_rank/continuous_median/Sorts.java
+++ src/main/java/aron/hacker_rank/continuous_median/Sorts.java
@@ -24,6 +24,33 @@ public class Sorts {
         }
     }
 
+    /*
+        [ 1, 2 ] = 2 / 1;
+            index 1 - 1 => 0;
+            index 1
+
+        [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 10 / 2
+            index 4
+            index 4 + 1
+
+        [ 1 , 2, 3 ] => len / 2 => 1
+        [1 , 2 , 3, 4, 5 ] => len / 2 => 2
+     */
+
+    public static double findMedian(final int[] arr) {
+        if ( arr.length == 1 ) return arr[0];
+
+        if ( arr.length % 2 == 0 ) {
+            int j = arr.length / 2;
+            int i = j - 1;
+
+            return (arr[i] + arr[j]) / 2.0;
+
+        } else {
+            return arr[arr.length / 2];
+        }
+    }
+
     public static int findMax(final int[] arr) {
         int n = arr[0];
         for ( int i : arr ) if ( i > n ) n = i;
